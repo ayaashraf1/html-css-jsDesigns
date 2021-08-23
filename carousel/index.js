6d74617de5f8ac265747a2c8d75749023161f5d6
Aya Ashraf <35771465+ayaashraf1@users.noreply.github.com>
2021-08-23 17:46:31 +0200

    Add files via upload

---
+++ carousel/index.js
@@ -0,0 +1,20 @@
+// const imagesArr =
+// ["https://images.unsplash.com/photo-1599394022918-6c2776530abb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1458&q=80"
+// ,"https://images.unsplash.com/photo-1593642632559-0c6d3fc62b89?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80"
+// ,"https://images.unsplash.com/photo-1599423300746-b62533397364?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80"
+// ,"https://images.unsplash.com/photo-1599561046251-bfb9465b4c44?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1492&q=80"];
+const conatainer = document.getElementById('imgContainer');
+const imgs = document.querySelectorAll("#carousel img");
+let index = 0;
+
+function changeImagesIndex(){
+    index++;
+    if(index > imgs.length-1){
+        index = 0;
+    }
+    conatainer.style.transform = `translateX(${-index * 300}px)`;
+}
+
+setInterval(() => {
+    changeImagesIndex();
+},2000);
---
+++ darkModeToggle/index.js
@@ -0,0 +1,7 @@
+const toggleBtn = document.getElementById('toggleBtn');
+const container = document.getElementById('container');
+
+toggleBtn.addEventListener('click',()=>{
+    debugger;
+container.classList.toggle('lightMode');
+});
